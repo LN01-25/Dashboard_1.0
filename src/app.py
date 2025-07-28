344c156970afc5eb6f496be3fdef57e4a86b6524
LN01-25 <l.e.nausester@gmail.com>
2025-07-28 22:34:33 +0200

    gruß_korr1.2

--- src/app.py
+++ src/app.py
@@ -19,14 +19,12 @@ username = st.session_state.get('username')
 
 # Login-Status prüfen
 if authentication_status:
-
-    if st.session_state.get("active_page") == "Start":
-
-        st.title("Hättest du das gedacht.... crazy oder??")
-        st.subheader("Dicken Kuss... viel Spaß morgen auf der Arbeit")
     
     sidebar.angemeldet_als()
     sidebar.navigation()
+    if st.session_state.get("active_page") == "Start":
+        st.title("Hättest du das gedacht.... crazy oder??")
+        st.subheader("Dicken Kuss... viel Spaß morgen auf der Arbeit")
     if st.session_state.get("active_page") == "Rezeptideen":
         rezepte.rezept1()
     if st.session_state.get("active_page") == "Dateideen":
